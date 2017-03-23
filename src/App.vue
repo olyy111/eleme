64cae220dfa464bcddc45e758f5c3f8b04697f1d
zl <sunrisehei@foxmail.com>
2017-03-23 21:52:04 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <eleme-header :seller="seller">
+      
+    </eleme-header>
+    <!--<nav>
+      
+    </nav>
+    <foods-info></foods-info>
+    <elem-counter>
+        <discount></discount>
+        <shop-cart></shop-cart>
+    </elem-counter>-->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import header from "./components/header/header"
+export default {
+  name: 'app',
+  data(){
+      return {
+        //数据还没来的时候子组件就渲染进去了，来了数据又更新视图
+        //如果值是null 在子组件用的时候就会报错
+          seller: {}
+      }
+  },
+  created(){
+      var _this = this
+      axios.get('/static/data.json')
+      .then(function (res){
+          _this.seller = res.data.seller
+      })
+  },
+  components: {
+    "eleme-header": header
+  }
+}
+</script>
+
+<style lang="stylus">
+    
+    
+    
+    
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,98 @@
+<template>
+    <section class="hd">
+        <div class="turn"></div>
+        <div class="seller-info clearfix">
+            <div class="avatar">
+                <img :src="seller.avatar">
+            </div>
+            <div class="intro">
+                <div class="title">
+                    {{seller.name}}
+                </div>
+                <p class="des">
+                    {{seller.description}}/{{seller.deliveryTime}}分钟送达
+                </p>
+                <p class="notice">
+                    {{seller.bulletin|wordsLengthLimit}}
+                </p>
+            </div>
+        </div>
+        <div class="activity-wrapper"></div>
+        <div class="bcg">
+            <img :src="seller.avatar">
+        </div>
+    </section>
+</template>
+<script>
+    export default {
+        props: {
+            seller: Object
+        },
+        data(){
+            return {
+                
+            }
+        },
+        methods: {
+        },
+        computed: {
+        },
+        filters: {
+            wordsLengthLimit(words){
+                if(words&&words.length > 20){
+                    var limitWords = words.slice(0, 21) + "..."
+                }
+                return limitWords || words
+            }
+        }
+    }
+</script>
+<style lang="stylus">
+    @import '../../common/css/tools.stylus'
+    .hd 
+        position: relative
+        height: 400px
+        padding: 0 45px
+        box-sizing: border-box
+        color: #fff
+        background-color: rgba(7,17,27,0.5)
+        background-size: 100% 100%
+        overflow: hidden
+    .turn
+        padding: 39px 0
+    .seller-info
+        .title
+            font-size: 48px
+            line-height: 88px
+        .avatar
+            float: left 
+            width: 240px
+            height: 240px
+            margin-right: 47px
+            font-weight: bold
+            img 
+                width: 100%
+                height: 100%
+        .intro 
+            float: left  
+        .des 
+            font-size: 36px
+            line-height: 44px   
+            margin-bottom: 43px
+        .notice 
+            font-size: 36px
+            line-height: 65px 
+    .bcg 
+        position: absolute
+        z-index: -1
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        filter: blur(24px)
+        img
+            width: 100%
+            height: 100%
+            
+    
+</style>
