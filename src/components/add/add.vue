d49696ed98c2e35d7f2475747eb204a9e123720d
zl <sunrisehei@foxmail.com>
2017-03-27 14:57:19 +0800

    good页数据处理

--- src/components/add/add.vue
+++ src/components/add/add.vue
@@ -1,12 +1,50 @@
 <template>
     <div class="add-cart">
-        <span class="reduce icon-remove_circle_outline"></span>
-        <span class="counter">1</span>
-        <span class="plus icon-add_circle"></span>
+        <transition name="slide">
+            <div class="reduce-wrapper" v-show="food.count>0">
+                <span class="reduce icon-remove_circle_outline" @click="reduce"></span>
+            </div>
+        </transition>
+        <span class="counter">{{food.count}}</span>
+        <span class="plus icon-add_circle" @click="add"></span>
     </div>
 </template>
 <script>
     export default {
+        props:{
+            food: {
+                type: Object,
+                default(){
+                    return {}
+                }
+            }
+        },
+        methods: {
+            // add(){
+            //     this.$store.commit('add', this.food)
+            // },
+            // reduce(){
+            //     this.$store.commit('reduce', this.food)
+            // }
+            add(){
+                if(!this.food.count){
+                    this.$set(this.food, 'count', 1)
+                }else {
+                    this.food.count ++
+                }
+            },
+            reduce(){
+                if(this.food.count){
+                    this.food.count --
+                    if(this.food.count === 0){
+                        delete this.food.count
+                    }
+                }
+            }
+        },
+        computed: {
+            
+        }
 
     }
 </script>
@@ -14,13 +52,32 @@
     .add-cart
         diaplay: flex
         align-items: center
-        .reduce
+        .reduce-wrapper
             display: inline-block
-            color: #3190e8
+            .reduce
+                display: inline-block
+                padding: 20px
+                color: #3190e8
         .plus
+            display: inline-block
+            right: 0
             color: #3190e8
+            padding: 20px
         .counter
             display: inline-block
             font-size: 34px
             vertical-align: 20px
+        .slide-enter-active, .slide-leave-active 
+            transition: all .3s ease
+            transform: translate3d(0, 0, 0)
+            .reduce
+                transform: rotate(0)
+                transition: all .3s ease
+        .slide-enter, .slide-leave-active 
+            transform: translate3d(130px, 0, 0)
+            .reduce
+                transform: rotate(180deg)
+                opacity: 0
+            
+    
 </style>
