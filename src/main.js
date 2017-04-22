49040f3bede35ff288b625cb4b0c304a16feb7bf
zl <sunrisehei@foxmail.com>
2017-04-22 09:44:04 +0800

    清楚无用文件

--- src/main.js
+++ src/main.js
@@ -4,58 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
-import BScroll from 'better-scroll'
 import vueTap from 'v-tap';
 import vscroll from 'v-scroll'
 import store from './store/index.js'
+import Velocity from "velocity-animate"
 Vue.config.productionTip = false
+
 Vue.use(vueTap);
-Vue.use(Vuex)
 Vue.use(vscroll)
 
-// var store = new Vuex.Store({
-//   state:{
-//     products: []
-//   },
-//   getters: {
-//     foodsNum(state){
-//       var num = 0
-//       state.products.forEach( food => {
-//         num += food.count
-//       })
-//       return num
-//     }
-//   },
-//   mutations: {
-//       add(state, food){
-//           var product = state.products.find( product => product === food)
-//           if(!product){
-//             product = food
-//             Vue.set(product, 'count', 1)
-//             state.products.push(product)
-//           }else {
-//             product.count ++
-//           }
-//       },
-//       reduce(state, food){
-//         var product = state.products.find( product => product === food)
-//         if(product){
-//             product.count --
-//             if(product.count === 0){
-//                 var index = state.products.findIndex( product => product === food)
-//                 state.products.splice(index, 1)
-//             }
-//         }
-//       },
-//       clear(state){
-//         state.products.forEach( food => {food.count = 0})
-//         state.products = []
-//       }
-//   }
-
-// })
-
 var eventHub = new Vue()
+
 new Vue({
   router,
   store,
